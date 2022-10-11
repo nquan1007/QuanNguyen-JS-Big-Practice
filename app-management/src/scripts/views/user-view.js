9f783888fe2f63284177b93a3d43a6cd071e16d9
Quan Nguyen <nquan1007@gmail.com>
2022-10-11 15:10:03 +0700

    #5 Add eventListener to show and hide Login Signup Form

--- app-management/src/scripts/views/user-view.js
+++ app-management/src/scripts/views/user-view.js
@@ -11,12 +11,26 @@ export default class UserView {
 
   queryElements = () => {
     this.btnLogin = document.getElementById('btn-login');
-    // ...
+    this.loginPopup = document.querySelector('.login-popup');
+    this.btnLoginCloseIcon = document.getElementById('login-form-close');
+    this.btnSignup = document.getElementById('btn-signup');
+    this.signupPopup = document.querySelector('.signup-popup');
+    this.btnSignupCloseIcon = document.getElementById('signup-form-close');
   }
 
   bindEventListeners = () => {
-    this.btnLogin.addEventListener('click', this.handleBtnLoginClick);
-    // ...
+    this.btnLogin.addEventListener('click', () => {
+      this.loginPopup.style.display = 'flex';
+    });
+    this.btnLoginCloseIcon.addEventListener('click', () => {
+      this.loginPopup.style.display = 'none';
+    });
+    this.btnSignup.addEventListener('click', () => {
+      this.signupPopup.style.display = 'flex';
+    });
+    this.btnSignupCloseIcon.addEventListener('click', () => {
+      this.signupPopup.style.display = 'none';
+    });
   }
 
   // UI logic below
