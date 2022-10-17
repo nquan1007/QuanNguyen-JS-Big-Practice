a5230a2096153f61e19f1193dd0ee5b769a4670b
Quan Nguyen <nquan1007@gmail.com>
2022-10-17 17:20:09 +0700

    #10 Finish handling events to show and hide Login & Register forms

--- product-management/src/scripts/views/index-view.js
+++ product-management/src/scripts/views/index-view.js
@@ -1,7 +1,5 @@
 export default class IndexView {
-  constructor() {
-
-  }
+  constructor() {}
 
   initialize = () => {
     this.queryElements();
@@ -9,20 +7,29 @@ export default class IndexView {
   };
 
   queryElements = () => {
-    this.btnLogin = document.querySelector('#register-form .btn-redirect');
-    this.loginForm = document.getElementById('login-form');
-    this.registerForm = document.getElementById('register-form');
-    console.log(this.btnLogin, this.loginForm, this.registerForm);
+    this.btnLogin = document.querySelector("#register-form .btn-redirect");
+    this.btnRegister = document.querySelector('#login-form .btn-redirect');
+    this.loginForm = document.getElementById("login-form");
+    this.registerForm = document.getElementById("register-form");
   };
 
   bindEventListeners = () => {
     /**
      * Click the Login button to show the Login form and hide the Register form
      */
-    this.btnLogin.addEventListener("click", () => {
+    this.btnLogin.addEventListener("click", (e) => {
+      e.preventDefault();
       this.loginForm.style.display = "block";
       this.registerForm.style.display = "none";
     });
-  };
 
+    /**
+     * Click the Register button to show the Register form and hide the Login form
+     */
+    this.btnRegister.addEventListener("click", (e) => {
+      e.preventDefault();
+      this.loginForm.style.display = "none";
+      this.registerForm.style.display = "block";
+    });
+  };
 }
