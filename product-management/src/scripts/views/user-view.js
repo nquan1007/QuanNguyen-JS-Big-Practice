65ac2819f9fe23fb02f81a0c7146dbd314d91a56
QuanNguyen <nquan1007@gmail.com>
2022-10-18 11:18:37 +0700

    #37 Update the methods to show and hide elements

--- product-management/src/scripts/views/user-view.js
+++ product-management/src/scripts/views/user-view.js
@@ -8,40 +8,42 @@ export default class UserView {
 
   queryElements = () => {
     this.btnRegister = document.getElementById("btn-login-redirect");
-    this.btnLogin = document.getElementById('btn-register-redirect');
+    this.btnLogin = document.getElementById("btn-register-redirect");
+    this.loginForm = document.getElementById("login-form");
+    this.registerForm = document.getElementById("register-form");
   };
 
   bindEventListeners = () => {
     // Click the Login button to show the Login form and hide the Register form
-    this.btnLogin.addEventListener("click", (e) => {
-      e.preventDefault();
-      this.bindShowElement('login-form');
-      this.bindHideElement('register-form');
-    });
+    this.btnLogin.addEventListener("click", (e) => this.showLoginForm(e));
 
     // Click the Register button to show the Register form and hide the Login form
-    this.btnRegister.addEventListener("click", (e) => {
-      e.preventDefault();
-      this.bindShowElement('register-form');
-      this.bindHideElement('login-form');
-    });
+    this.btnRegister.addEventListener("click", (e) => this.showRegisterForm(e));
+  };
+
+  // Handle to show Register Form
+  showRegisterForm = (e) => {
+    e.preventDefault();
+    this.showElement(this.registerForm);
+    this.hideElement(this.loginForm);
+  };
+
+  // Handle to show Login Form
+  showLoginForm = (e) => {
+    e.preventDefault();
+    this.showElement(this.loginForm);
+    this.hideElement(this.registerForm);
   };
 
   /**
-   * Pass the DOM by id of the element to show it
-   * @param {DOM by ID} dom 
+   * Pass the element to show it
+   * @param {*} element
    */
-  bindShowElement = (dom) => {
-    this.element = document.getElementById(dom);
-    this.element.style.display = "block";
-  }
+  showElement = (element) => (element.style.display = "block");
 
   /**
-   * Pass the DOM by id of the element to show it
-   * @param {DOM by ID} dom 
+   * Pass the element to hide it
+   * @param {*} element
    */
-   bindHideElement = (dom) => {
-    this.element = document.getElementById(dom);
-    this.element.style.display = "none";
-  }
+  hideElement = (element) => (element.style.display = "none");
 }
