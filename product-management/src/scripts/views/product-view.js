c7086c13fe3e99aa0974b97805f755cfe338fc9a
Quan Nguyen <nquan1007@gmail.com>
2022-10-26 14:16:32 +0700

    #19 Handle events to show and hide Add new form

--- product-management/src/scripts/views/product-view.js
+++ product-management/src/scripts/views/product-view.js
@@ -1,4 +1,4 @@
-import { showFlexElement, hideElement } from "../helpers/view-utilities";
+import { showFlexElement, hideElement, showElement } from "../helpers/view-utilities";
 
 export default class ProductView {
   constructor() {}
@@ -12,17 +12,33 @@ export default class ProductView {
     this.avatarIcon = document.getElementById('avatar-icon')
     this.userBox = document.getElementById('user-box');
     this.btnLogout = document.getElementById('btn-logout');
+
+    this.popupAddForm = document.getElementById('popup-add-form');
+    this.btnAddNew = document.getElementById('btn-add-new');
+    this.btnCloseAddForm = document.getElementById('popup-add-form-close');
   }
 
   bindEventListeners = () => {
+    // Click the Logout button to redirect to the Index Page
+    this.btnLogout.addEventListener('click', this.logout);
+
     // Click the Avatar Icon to show User Box 
     this.avatarIcon.addEventListener('click', this.showUserBox);
 
     // Click out of the User Box and Avatar Icon to hide User Box
     document.addEventListener('mouseup', this.hideUserBox);
 
-    // Click the Logout button to redirect to the Index Page
-    this.btnLogout.addEventListener('click', this.logout);
+    // Click the Add new product button to show the Add new form Popup
+    this.btnAddNew.addEventListener('click', this.showAddForm);
+
+    // Click the Close button in the Add new form to close it
+    this.btnCloseAddForm.addEventListener('click', this.hideAddForm);
+  }
+
+  // Handle to logout to the Index Page
+  logout = (e) => {
+    e.preventDefault();
+    window.location.assign('./index.html');
   }
 
   // Handle to show User Box
@@ -37,10 +53,14 @@ export default class ProductView {
       hideElement(this.userBox);
     }
   }
-
-  // Handle to logout to the Index Page
-  logout = (e) => {
+  
+  showAddForm = (e) => {
     e.preventDefault();
-    window.location.assign('./index.html');
+    showFlexElement(this.popupAddForm);
+  }
+
+  hideAddForm = (e) => {
+    e.preventDefault();
+    hideElement(this.popupAddForm);
   }
 }
